5bb4e00fa2f54874929bd82bfa9561cc8fdc0c46
Nathan Robinson <nrobinson2000@me.com>
2015-07-22 16:10:56 +0100

    Initial Commit

---
+++ SparkBot Remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SparkBot Remote
+//
+//  Created by Nathan Robinson on 7/22/15.
+//  Copyright (c) 2015 Nathan Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
